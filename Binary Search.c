eb9bd8c4d2ae2e20d39938c7ad660d133320c9ae
apurvawani <32008712+apurvawani@users.noreply.github.com>
2017-09-18 20:55:24 +0530

    Binary Search in an array
    This program search a particular element in an array using binary search technique.

---
+++ Binary Search.c
@@ -0,0 +1,33 @@
+/*Write a program to store N numbers in a sorted array and search particular number.*/
+
+#include<stdio.h>
+void main(){
+	int i, N, r;
+	printf("Enter the size of an array:\n");
+	scanf("%d",&N);
+	int s[N];
+	printf("Enter the sorted array:\n");
+	for(i=0;i<N;i++){
+		scanf("%d",&s[i]);
+	}
+	printf("Enter the search element:\n");
+	scanf("%d",&r);
+	int top=0, bot=N-1, mid;
+	mid=(top+bot)/2;
+	while(top<=bot){
+		if(s[mid]==r){
+			printf("%d is at %d position in the array.\n",r,(mid+1));
+			break;
+		}
+		else{
+			if(s[mid]>r){
+				bot=mid-1;
+				mid=(top+bot)/2;
+			}
+			else{
+				top=mid+1;
+				mid=(top+bot)/2;
+			}
+		}
+	}
+}
